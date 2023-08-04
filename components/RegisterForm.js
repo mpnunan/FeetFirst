f4f5d8c32f49f159f5f7ca8d94084e9aecb1cba2
Mark Hamilton <ham.mark126@gmail.com>
2023-08-03 19:35:52 -0500

    user register form

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,26 +1,92 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useRouter } from 'next/router';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
+import { getSingleUser } from '../api/userData';
 
 function RegisterForm({ user, updateUser }) {
-  const [formData, setFormData] = useState({
-    bio: '',
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    profileImageUrl: '',
+    userName: '',
     uid: user.uid,
-  });
+  };
+  const [formData, setFormData] = useState(initialState);
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (user.uid) {
+      getSingleUser(id).then((userObj) => {
+        setFormData((prevState) => ({
+          ...prevState,
+          id: userObj.id,
+          firstName: userObj.first_name,
+          lastName: userObj.last_name,
+          email: userObj.email,
+          userName: userObj.username,
+          profileImageUrl: userObj.profile_image_url,
+        }));
+      });
+    }
+  }, [user, id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    registerUser(formData).then(() => updateUser(user.uid));
+    if (formData.id) {
+      const customer = {
+        id: formData.id,
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        userName: formData.userName,
+        profileImageUrl: formData.profileImageUrl,
+        uid: user.uid,
+      };
+      updateUser(customer, user.uid).then(() => router.push('/profile'));
+    } else {
+      registerUser(formData).then(() => updateUser(user.uid));
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>First Name</Form.Label>
+        <Form.Control as="textarea" name="firstName" required value={formData.firstName} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control as="textarea" name="lastName" required value={formData.lastName} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Username</Form.Label>
+        <Form.Control as="textarea" name="userName" required value={formData.userName} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" name="email" required value={formData.email} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Profile Image</Form.Label>
+        <Form.Control type="url" name="profileImageUrl" required value={formData.profileImageUrl} onChange={handleChange} />
+        <Form.Text className="text-muted" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
