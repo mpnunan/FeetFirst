005d33a046112109848e5040377a8a7a636d42a2
David Poole <dbpoole7@gmail.com>
2023-08-05 10:55:17 -0500

    user issue on shop page not displaying products fixed

--- pages/shop.js
+++ pages/shop.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { getAllProducts } from '../api/productData';
 import ShoeCard from '../components/shoeCard';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Shop() {
+  const { user } = useAuth();
   // SET A STATE FOR THE SHOE CARDS
   const [shoes, setShoes] = useState([]);
 
   // API FUNCTION TO GET ALL SHOE CARDS
   const getAllShoes = () => {
-    getAllProducts().then(setShoes);
+    getAllProducts(user.uid).then(setShoes);
   };
   console.warn(shoes);
 
@@ -25,7 +27,7 @@ export default function Shop() {
         <div className="d-flex flex-wrap">
           {/* MAPS OVER SHOES USING THE SHOECARD COMPONENT */}
           {shoes.map((shoe) => (
-            <ShoeCard key={shoe.firebaseKey} shoeObj={shoe} onUpdate={getAllShoes} />
+            <ShoeCard key={shoe.id} shoeObj={shoe} onUpdate={getAllShoes} />
           ))}
         </div>
       </div>
