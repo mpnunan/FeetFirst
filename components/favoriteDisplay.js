b5c6252659638d75e5cf90650f0afd77c1268171
Michael Nunan <michael.nunan@outlook.com>
2023-08-05 13:55:42 -0500

    favorites accordian

--- components/favoriteDisplay.js
+++ components/favoriteDisplay.js
@@ -1,10 +1,26 @@
+import React, { useEffect, useState } from 'react';
 import { Accordion } from 'react-bootstrap';
 import AccordionRow from './AccordionRow';
+import { useAuth } from '../utils/context/authContext';
+import getFavoriteProducts from '../api/favoriteData';
 
 export default function FavoriteDisplay() {
+  const { user } = useAuth();
+  const [favorites, setFavorites] = useState([]);
+
+  const getFavorites = () => {
+    getFavoriteProducts(user.uid).then(setFavorites);
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
   return (
     <Accordion>
-      <AccordionRow />
+      {favorites.map((favorite) => (
+        <AccordionRow key={favorite.id} favoriteProduct={favorite} onUpdate={getFavorites} />
+      ))}
     </Accordion>
   );
 }
