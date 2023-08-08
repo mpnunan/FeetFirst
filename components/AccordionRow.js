9fc26aa4dc7f9928fbcfd403c9f5fb6e25cf790e
Michael Nunan <michael.nunan@outlook.com>
2023-08-07 19:09:57 -0500

    fixed console error for button nesting

--- components/AccordionRow.js
+++ components/AccordionRow.js
@@ -15,18 +15,19 @@ export default function AccordionRow({ favoriteProduct, onUpdate, keyNumber }) {
       <Accordion.Header className="favoritesCardContainer">
         <Image className="favoritePic" src={favoriteProduct.image_url} />
         <div className="favoritesCardHeader">{favoriteProduct.title}</div>
+      </Accordion.Header>
+      <Accordion.Body>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
         <div className="favBtnContainer">
           <Button className="favBtn" variant="dark" onClick={removeFavorite}>Remove Favorite</Button>
         </div>
-      </Accordion.Header>
-      <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
       </Accordion.Body>
     </Accordion.Item>
   );
