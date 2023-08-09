4a091b9f91c61cad66be50c8f81dbce3fbc59f97
Mark Hamilton <ham.mark126@gmail.com>
2023-08-08 19:30:52 -0500

    minor styling

--- components/shoeCard.js
+++ components/shoeCard.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Link from 'next/link';
 import { Heart, HeartFill } from 'react-bootstrap-icons';
+import { useRouter } from 'next/router';
 import { createFavoriteProduct, deleteFavoriteProduct } from '../api/favoriteData';
 import { useAuth } from '../utils/context/authContext';
 
 function ShoeCard({ shoeObj, onUpdate }) {
   const { user } = useAuth();
+  const router = useRouter();
+  const itemClick = () => {
+    if (shoeObj.id) {
+      router.push(`/products/${shoeObj.id}`);
+    }
+  };
   const favButton = () => createFavoriteProduct(shoeObj.id, user.uid).then(() => onUpdate());
   const unFavButton = () => deleteFavoriteProduct(shoeObj.id, user.uid).then(() => onUpdate());
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={shoeObj.image_url} alt={shoeObj.title} style={{ height: '400px' }} />
-      <Card.Body>
-        <Card.Title>{shoeObj.title}</Card.Title>
-        <Link href={`/products/${shoeObj.id}`} passHref>
-          <Button variant="primary" className="view-btn">VIEW</Button>
-        </Link>
+      <Card.Img variant="top" src={shoeObj.image_url} alt={shoeObj.title} onClick={itemClick} style={{ height: '400px' }} />
+      <Card.Body className="product-content" onClick={itemClick} style={{ marginTop: '0px' }}>
+        <Card.Title className="view-btn">
+          {shoeObj.title}
+        </Card.Title>
+      </Card.Body>
+      <div className="heart-btn">
         {shoeObj.favorite ? (
-          <HeartFill variant="button" onClick={unFavButton} className="unfav" />
+          <HeartFill variant="button" fill="red" onClick={unFavButton} className="unfav" />
         )
           : (
             <Heart variant="button" onClick={favButton} className="fav" />
           )}
-      </Card.Body>
+      </div>
     </Card>
   );
 }
