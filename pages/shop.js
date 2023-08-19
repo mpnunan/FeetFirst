38b73101cb5435f44412879f976cdd8f85ed7996
Michael Nunan <126629756+mpnunan@users.noreply.github.com>
2023-08-19 11:42:31 -0500

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -7,16 +7,13 @@ import { useAuth } from '../utils/context/authContext';
 
 export default function Shop() {
   const { user } = useAuth();
-  // SET A STATE FOR THE SHOE CARDS
   const [shoes, setShoes] = useState([]);
 
-  // API FUNCTION TO GET ALL SHOE CARDS
   const getAllShoes = () => {
     getAllProducts(user.uid).then(setShoes);
   };
   console.warn(shoes);
 
-  // API CALL TO GET ALL THE SHOES ON COMPONENT RENDER
   useEffect(() => {
     document.title = 'Feet First';
     getAllShoes();
@@ -29,7 +26,6 @@ export default function Shop() {
       </Head>
       <div className="text-center my-4">
         <div id="shopCards" className="d-flex flex-wrap">
-          {/* MAPS OVER SHOES USING THE SHOECARD COMPONENT */}
           {shoes.map((shoe) => (
             <ShoeCard key={shoe.id} shoeObj={shoe} onUpdate={getAllShoes} />
           ))}
